13cf7845c4d0a6eaa551d11e340792d1d48937ed
史 翔新 <x_shi@yumemi.co.jp>
2020-05-06 03:49:26 +0900

    Implement the main logic

--- Sources/Agen/main.swift
+++ Sources/Agen/main.swift
@@ -1,2 +1,31 @@
 import Cocoa
 import AgenLib
+
+let arguments = CommandLine.arguments
+let parser = Parser()
+
+do {
+    let result = try parser.parse(arguments)
+    switch result {
+    case .showHelp:
+        printHelp()
+        
+    case .showVersion:
+        printVersion()
+        
+    case .shortenURLString(let result):
+        let shortenURLString = result.urlString
+        print("Shorten URL: \(shortenURLString)")
+        if result.settings.shouldCopyToPasteboard {
+            NSPasteboard.general.replacePasteboardItem(with: shortenURLString)
+            print("Result has been copied to your clipboard, you can use cmd + v to paste it.")
+        }
+    }
+    
+    print()
+    exit(EXIT_SUCCESS)
+    
+} catch let error {
+    printError(error)
+    exit(EXIT_FAILURE)
+}
